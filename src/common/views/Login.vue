98c95beb61373f2d54a15e5abb26da61ae162b91
yanglin <lin.yang.neu@neusoft.com>
2024-03-01 14:24:49 +0800

    build: 📦️ 添加prettier tailwind、import插件

--- src/common/views/Login.vue
+++ src/common/views/Login.vue
@@ -1,22 +1,21 @@
 <template>
-  <div class="w-screen h-screen overflow-hidden relative">
+  <div class="relative h-screen w-screen overflow-hidden">
     <div>
       <img src="@/assets/img/bg.jpg" alt="" class="w-screen" />
     </div>
-    <div class="absolute top-[20%] left-[50%] -translate-x-[50%]">
-      <Card class="w-[400px] bg-opacity-50!">
+    <div class="absolute left-[50%] top-[20%] -translate-x-[50%]">
+      <Card class="w-[400px] !bg-opacity-50">
         <CardHeader class="text-center">
           <CardTitle>Welcome</CardTitle>
         </CardHeader>
         <CardContent>
-          <div class="grid items-center w-full gap-4">
+          <div class="flex w-full flex-wrap items-center gap-4">
             <div class="flex w-full max-w-sm items-center gap-1.5">
-              <Input id="name" v-model:model-value="email" placeholder="邮箱前缀" class="w-[200px]" />
-              <div>@reachauto.com</div>
+              <Input id="name" v-model:model-value="email" placeholder="请输入邮箱" />
             </div>
             <div class="flex w-full max-w-sm items-center gap-1.5">
               <Input id="name" placeholder="请输入验证码" class="w-[200px]" />
-              <Button @click="getCaptcha"> 获取验证码 </Button>
+              <Button @click="getCaptcha" class="flex-1"> 获取验证码 </Button>
             </div>
           </div>
         </CardContent>
